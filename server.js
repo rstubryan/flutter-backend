9b8ed66214b56a19b10a53099e7c177f1a320e9d
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-12-30 22:56:13 +0700

    Remove mahasiswaController and update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mahasiswaController = require('./controllers/mahasiswaController');
 const matakuliahController = require('./controllers/matakuliahController');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-
-// Endpoint mahasiswa
-app.use('/mahasiswa', mahasiswaController);
 
 // Endpoint matakuliah
 app.use('/matakuliah', matakuliahController);
